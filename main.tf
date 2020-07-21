e74ad06707eb4bbf60a9feb85a7c2ec591489e9e
Stephen OBrien <stephen.obrien@f5.com>
2020-07-21 14:29:57 +0100

    commit

--- main.tf
+++ main.tf
@@ -76,9 +76,12 @@ resource "aws_security_group" "f5" {
 #Create the BIG-IP
 resource "aws_instance" "big-ip" {
   ami           = "ami-0fe284d68b7936ab6"
-  instance_type = "t2.medium"
+  instance_type = "m5.xlarge"
 
   tags = {
     Name = "HelloWorld"
+  }
+  tags = {
+    Name = "ob1-mybigip"
   }
 }
