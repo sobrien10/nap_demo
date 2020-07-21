ae27773c553721363b96e984cfbeea41e250092f
Stephen OBrien <stephen.obrien@f5.com>
2020-07-21 13:41:47 +0100

    Commit

--- main.tf
+++ main.tf
@@ -21,3 +21,50 @@ module "vpc" {
     Environment = "ob1-vpc-teraform"
   }
 }
+
+resource "aws_security_group" "f5" {
+  name   = "${var.prefix}-f5"
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["90.208.9.18/32"]
+  }
+
+    ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["90.208.9.18/32"]
+  }
+
+    ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["90.208.9.18/32"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["90.208.9.18/32"]
+  }
+
+  ingress {
+    from_port   = 8443
+    to_port     = 8443
+    protocol    = "tcp"
+    cidr_blocks = ["90.208.9.18/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["90.208.9.18/32"]
+  }
+}
