8bff200d7ac83efbc52a9c6090204072127c8b86
Stephen OBrien <stephen.obrien@f5.com>
2020-07-21 10:17:46 +0100

    Commit

---
+++ main.tf
@@ -0,0 +1,24 @@
+#Configure the AWS provider
+provider "aws" {
+  version = "~> 2.0"  
+  region  = "eu-west-2"
+}
+
+#Configure the VPC and Public Subnets
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> v2.0"
+
+  name = "${var.prefix}-f5-vpc"
+  cidr = "10.0.0.0/16"
+
+
+  azs             = ["eu-west-2a"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+
+  enable_nat_gateway = false
+
+  tags = {
+    Environment = "ob1-vpc-teraform"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "prefix" {
+  description = "prefix for resources created"
+  default     = "ob1-hashi-f5-demo"
+}
